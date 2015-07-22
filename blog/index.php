55542f9f11eea7f6592f3b274ff10ba8321ea972
xkvyor <xkvyor@gmail.com>
2015-07-22 13:43:45 +0800

    blog test

---
+++ blog/index.php
@@ -0,0 +1,17 @@
+<!-- index.html -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Hello React</title>
+    <script src="../assets/js/react.js"></script>
+    <script src="../assets/js/JSXTransformer.js"></script>
+    <script src="../assets/js/jquery.js"></script>
+  </head>
+  <body>
+    <div id="content"></div>
+    <script type="text/jsx">
+      // Your code here
+    </script>
+  </body>
+</html>
