7aadf6569cec8a9232565234ba5a39d28de57ef7
xkvyor <xkvyor@gmail.com>
2015-07-22 13:57:44 +0800

    add jq min

--- blog/index.php
+++ blog/index.php
@@ -4,14 +4,26 @@
   <head>
     <meta charset="UTF-8" />
     <title>Hello React</title>
-    <script src="../assets/js/react.js"></script>
+    <script src="../assets/js/react.min.js"></script>
     <script src="../assets/js/JSXTransformer.js"></script>
-    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/jquery.min.js"></script>
   </head>
   <body>
     <div id="content"></div>
     <script type="text/jsx">
-      // Your code here
+      var CommentBox = React.createClass({displayName: 'CommentBox',
+        render: function() {
+          return (
+            React.createElement('div', {className: "commentBox"},
+              "Hello, world!"
+            )
+          );
+        }
+      });
+      React.render(
+        React.createElement(CommentBox, null),
+        document.getElementById('content')
+      );
     </script>
   </body>
 </html>
